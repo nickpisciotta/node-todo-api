733ea871270fc0669d5e3caca9e5c99bd572f425
Nick Pisciotta <npisciotta@gmail.com>
2017-01-16 21:51:31 -0700

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo'); 
 
+const todos = [ {
+  text: "First todos"
+}, {
+  text: "Second todos"
+}]
 
 beforeEach((done) => {
-  Todo.remove({}).then(() => done()); 
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos)
+  }).then(() => done()); 
 }); 
 
 describe('POST /todos', () => {
@@ -23,7 +30,7 @@ describe('POST /todos', () => {
         if (err) {
           return done(err); 
         }
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text); 
           done(); 
@@ -42,9 +49,21 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0); 
+          expect(todos.length).toBe(2); 
           done(); 
         }).catch((err) => done()); 
       });  
+  }); 
+}); 
+
+describe('GET /todos', () => {
+  it("should get all Todos", (done) => {
+    request(app) 
+      .get("/todos")
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done); 
   }); 
 }); 
