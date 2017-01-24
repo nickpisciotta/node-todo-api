6b2c6d617156e95c5292bf67618401e0860ee1e3
Nick Pisciotta <npisciotta@gmail.com>
2017-01-24 12:10:21 -0700

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -95,5 +95,39 @@ describe('GET /toods/:id', () => {
       .expect(404)
       .end(done); 
   });
-  
 }); 
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString(); 
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200) 
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId); 
+      }).end((err) => {
+        if (err) {
+          return done(err); 
+        }
+        Todo.findById(hexId).then((todos) => {
+          expect(todos).toNotExist(); 
+          done(); 
+        }).catch((err) => done(err)); 
+      }); 
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    request(app) 
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done) 
+  }); 
+
+  it('should return a 404 if object id is invalid', (done) => { 
+    request(app) 
+      .delete('/todos/123')
+      .expect(404)
+      .end(done) 
+  }); 
+});
