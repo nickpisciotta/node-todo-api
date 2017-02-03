79560c142389b62457abc2295b572de8a14714eb
Nick Pisciotta <npisciotta@gmail.com>
2017-02-02 17:06:54 -0700

    Update config to use json file

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -12,7 +12,7 @@ const users = [{
   password: "userOnePass", 
   tokens: [{
     access: 'auth', 
-    token: jwt.sign({_id: userOneId, access: 'auth'}, "abc123").toString() 
+    token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString() 
   }]
 }, {
   _id: userTwoId, 
@@ -20,10 +20,9 @@ const users = [{
   password: "userTwoPass",
   tokens: [{
     access: 'auth', 
-    token: jwt.sign({_id: userOneId, access: 'auth'}, "abc123").toString() 
+    token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString() 
   }]
 }]; 
-
 
 const todos = [ {
   _id: new ObjectID(), 
@@ -42,7 +41,6 @@ const populateTodos = (done) => {
     return Todo.insertMany(todos);
   }).then(() => done()); 
 }; 
-
 
 const populateUsers = (done) => {
   User.remove({}).then(() => {
